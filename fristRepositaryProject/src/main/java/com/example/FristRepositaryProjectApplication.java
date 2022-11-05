bfa5e412c585d50b79a6db5721a9192015ba6065
kanherePatil179 <siddheshwardk07@gmail.com>
2022-11-05 12:05:20 +0530

    Frist Eclipce project

---
+++ fristRepositaryProject/src/main/java/com/example/FristRepositaryProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FristRepositaryProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FristRepositaryProjectApplication.class, args);
+	}
+
+}
